3148bf892f86647b0bd84e543edd8ba6964e26d3
ND-Joc <e.andres.jofre.correa@gmail.com>
2025-09-27 23:55:20 +0200

    Add files via upload

---
+++ js/closevideo.js
@@ -0,0 +1,16 @@
+//Start Stop Iframe
+$(function(){
+    $('.closeVideos').click(function(){
+		$("#project18").attr('src', $("#project18").attr('src'));
+		$("#project17").attr('src', $("#project17").attr('src'));
+		$("#project14").attr('src', $("#project14").attr('src'));
+		$("#project13").attr('src', $("#project13").attr('src'));
+        $("#project12").attr('src', $("#project12").attr('src'));
+		$("#project7").attr('src', $("#project7").attr('src'));
+		$("#project5").attr('src', $("#project5").attr('src'));
+        $("#project4").attr('src', $("#project4").attr('src'));
+        $("#project2").attr('src', $("#project2").attr('src'));
+		
+    });
+});
+//End Stop Iframe 
